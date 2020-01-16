9bc2e7881a465517a4b2b3ed6e9bdb6b1fd6b1bf
Luciano Júnior <luciannojunior@gmail.com>
2020-01-16 10:47:55 -0300

    Adding type

--- course/writing-css/index.ts
+++ course/writing-css/index.ts
@@ -3,7 +3,7 @@ export default {
     {
       description: 'First test of SECOND step',
       failMsg: 'You can also throw custom error messages. This will be the default message sent.',
-      test: async({ files, getFile, lib }) => {
+      test: async({ getFile }: any) => {
         const pdp = await getFile('store/blocks/product.jsonc') 
         return !!pdp
       }
--- course/writing-jsonc/index.ts
+++ course/writing-jsonc/index.ts
@@ -1,9 +1,9 @@
 export default {
   tests: [
     {
-      description: 'First test of this step',
+      description: 'First TEeT of this step',
       failMsg: 'You can also throw custom error messages. This will be the default message sent.',
-      test: async({ files, getFile, lib }) => {
+      test: async({ getFile }: any) => {
         const pdp = await getFile('store/blocks/product.jsonc') 
         return !!pdp
       }
