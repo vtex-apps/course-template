89825e13476d9257e1f12c6b2271e54d897355a7
Luciano Júnior <luciannojunior@gmail.com>
2020-03-03 10:19:32 -0300

    Adding clients and testing stuff

--- course/index.ts
+++ course/index.ts
@@ -1,32 +1,116 @@
-import { IOClients, Service, ServiceContext } from "@vtex/api";
-import { join } from 'path'
+import {
+  IOClients,
+  ClientsConfig,
+  Service,
+  ServiceContext,
+  UserInputError,
+  InstanceOptions,
+  IOContext,
+  AppClient,
+  LRUCache
+} from "@vtex/api";
+import { json } from "co-body";
+import { join } from "path";
 
-declare global {
-  type Context = ServiceContext<IOClients, {}>;
+interface GetFileArgs {
+  installationId: string;
+  owner: string;
+  repo: string;
+  branch: string;
+  path: string;
 }
 
-const correctionHandler = async (ctx: Context) => {
-  const step = ctx.query.step;
+class CourseHubClient extends AppClient {
+  private routes = {
+    file: () => `/app/coursehub/file`
+  };
 
-  const stepModule = await import(join(__dirname, `./${step}.js`));
+  constructor(ioContext: IOContext, options?: InstanceOptions) {
+    super("vtex.course-hub@0.x", ioContext, { ...options });
+  }
+
+  public getFileContent({
+    installationId,
+    owner,
+    repo,
+    branch,
+    path
+  }: GetFileArgs) {
+    return this.http.get<string>(this.routes.file(), {
+      params: { installationId, owner, repo, branch, path }
+    });
+  }
+}
+
+class Clients extends IOClients {
+  public get courseHub() {
+    return this.getOrSet("courseHub", CourseHubClient);
+  }
+}
+
+const TIMEOUT_MS = 10000;
+
+const memoryCache = new LRUCache<string, any>({ max: 5000 });
+metrics.trackCache("status", memoryCache);
+
+const clients: ClientsConfig<Clients> = {
+  implementation: Clients,
+  options: {
+    default: {
+      retries: 2,
+      timeout: TIMEOUT_MS
+    }
+  }
+};
+
+declare global {
+  type Context = ServiceContext<Clients>;
+}
+
+/**
+ * Resolves the corresponding name for a branch to search the code depending on the step.
+ *
+ * As of now, we require for it to be equal to the step 'folder'
+ */
+const getBranchForStep = (step: string) => step;
+
+const correctionHandler = async (ctx: Context) => {
+  const { step, owner, repo, installationId } = await json(ctx.req);
+  const {
+    clients: { courseHub }
+  } = ctx;
+
+  let stepModule = null;
+  try {
+    stepModule = await import(join(__dirname, `./${step}.js`));
+  } catch (e) {
+    // TODO: Return an { error } maybe
+    throw new UserInputError(`There's no step '${step}' on this course.`);
+  }
+
   const suite = stepModule.default;
 
   const results = await Promise.all(
     suite.tests.map(async ({ test, description, failMsg }: any) => {
       try {
-        const pass = await test({
+        const success = await test({
           files: [],
-          getFile: (name: string) => new Promise(resolve => resolve(name)),
-          lib: { }
+          getFile: (path: string) =>
+            courseHub.getFileContent({
+              installationId,
+              owner,
+              repo,
+              branch: getBranchForStep(step),
+              path
+            }),
+          lib: {}
         });
         return {
-          pass,
-          ...{
-            failMsg: pass ? undefined : failMsg,
-            successMsg: description
-          }
+          success,
+          msg: success ? description : failMsg
         };
       } catch (e) {
+        throw e;
         return { pass: false, failMsg: e.message || failMsg };
       }
     })
@@ -37,7 +121,8 @@ const correctionHandler = async (ctx: Context) => {
   ctx.body = results;
 };
 
-export default new Service<any, any>({
+export default new Service<Clients>({
+  clients,
   routes: {
     correction: [correctionHandler]
   }
--- course/writing-jsonc/index.ts
+++ course/writing-jsonc/index.ts
@@ -4,8 +4,8 @@ export default {
       description: 'First TEeT of this step',
       failMsg: 'You can also throw custom error messages. This will be the default message sent.',
       test: async({ getFile }: any) => {
-        const pdp = await getFile('store/blocks/product.jsonc') 
-        return !!pdp
+        const pdp = await getFile('store/blocks/product.jsonc')
+        return pdp.name === 'lucis'
       }
     }
   ]
